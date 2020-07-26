67a63b53121856107c0c643c37470d3598a7e611
zzacong <30360914@students.federation.edu.au>
2020-07-26 19:12:34 +1000

    v1.2

--- App.js
+++ App.js
@@ -1,24 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  // TouchableWithoutFeedback,
+  // TouchableOpacity,
+  // TouchableHighlight,
+  TouchableNativeFeedback,
+  View,
+  Image,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
-  let x = 1;
-  console.log("App executed, Yo!");
+  // const handlePress = () => console.log("Text pressed");
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      {/* <Text numberOfLines={1} onPress={() => console.log("Text pressed 1")}> */}
+      {/* <Text numberOfLines={1} onPress={handlePress}> */}
       <Text>Hello React Native</Text>
+      <TouchableHighlight onPress={() => console.log("Image tapped")}>
+        {/* <Image source={require("./assets/icon.png")} /> */}
+        <Image
+          // blurRadius={10}
+          // fadeDuration={1000}
+          style={styles.logo}
+          source={{ width: 200, height: 200, uri: "https://picsum.photos/200" }}
+        />
+      </TouchableHighlight>
+      <TouchableNativeFeedback onPress={() => console.log("Image tapped")}>
+        <View
+          style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}
+        ></View>
+      </TouchableNativeFeedback>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
