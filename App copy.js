fc4418d0a49a6383ad81fb73577ba2f07c757446
zzacong <30360914@students.federation.edu.au>
2020-07-27 23:13:03 +1000

    v1.4

---
+++ App copy.js
@@ -0,0 +1,94 @@
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  // Dimensions,
+  StyleSheet,
+  // Text,
+  // TouchableWithoutFeedback,
+  // TouchableOpacity,
+  // TouchableHighlight,
+  // TouchableNativeFeedback,
+  View,
+  // Image,
+  SafeAreaView,
+  // Button,
+  // Alert,
+  Platform,
+  StatusBar,
+} from "react-native";
+import {
+  // useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
+
+export default function App() {
+  // const handlePress = () => console.log("Text pressed");
+  // console.log(Platform.OS, Dimensions.get("screen"));
+  // console.log(Platform.OS, useDimensions());
+  // console.log(Platform.OS, useDeviceOrientation());
+
+  const { landscape } = useDeviceOrientation();
+  return (
+    // <SafeAreaView style={styles.container}>
+    //   {/* <Text numberOfLines={1} onPress={() => console.log("Text pressed 1")}> */}
+    //   {/* <Text numberOfLines={1} onPress={handlePress}> */}
+    //   <Text>Hello React Native</Text>
+    //   <StatusBar style="auto" />
+    // </SafeAreaView>
+    // <SafeAreaView style={styles.container}>
+    //   <TouchableHighlight onPress={() => console.log("Image tapped")}>
+    //     {/* <Image source={require("./assets/icon.png")} /> */}
+    //     <Image
+    //       blurRadius={2}
+    //       // fadeDuration={1000}
+    //       style={styles.logo}
+    //       source={{ width: 200, height: 200, uri: "https://picsum.photos/200" }}
+    //     />
+    //   </TouchableHighlight>
+    //   <TouchableNativeFeedback onPress={() => console.log("Image tapped")}>
+    //     <View
+    //       style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}
+    //     ></View>
+    //   </TouchableNativeFeedback>
+    //   <StatusBar style="auto" />
+    // </SafeAreaView>
+    // <SafeAreaView style={[styles.container, containerStyle]}>
+    //   <Button
+    //     color="dimgrey"
+    //     title="Click Me"
+    //     // onPress={() =>
+    //     //   Alert.alert("My title", "My message", [
+    //     //     { text: "Yes", onPress: () => console.log("Yes") },
+    //     //     { text: "No", onPress: () => console.log("No") },
+    //     //   ])
+    //     // }
+    //     onPress={() =>
+    //       Alert.prompt("My title", "My message", (text) => console.log(text))
+    //     }
+    //   />
+    //   <StatusBar style="auto" />
+    // </SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          // width: 150,
+          width: "100%",
+          height: landscape ? "100%" : "30%",
+        }}></View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+    // paddingTop: Platform.OS === "android" ? 20 : 0,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+});
+
+const containerStyle = { backgroundColor: "orange" };
--- App.js
+++ App.js
@@ -1,85 +1,62 @@
-// import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StyleSheet,
-  // Text,
-  // TouchableWithoutFeedback,
-  // TouchableOpacity,
-  // TouchableHighlight,
-  // TouchableNativeFeedback,
-  View,
-  // Image,
-  SafeAreaView,
-  Button,
-  Alert,
-  Platform,
-  StatusBar,
-} from "react-native";
+import { View } from "react-native";
 
 export default function App() {
-  // const handlePress = () => console.log("Text pressed");
-
   return (
-    // <SafeAreaView style={styles.container}>
-    //   {/* <Text numberOfLines={1} onPress={() => console.log("Text pressed 1")}> */}
-    //   {/* <Text numberOfLines={1} onPress={handlePress}> */}
-    //   <Text>Hello React Native</Text>
-    //   <StatusBar style="auto" />
-    // </SafeAreaView>
-    // <SafeAreaView style={styles.container}>
-    //   <TouchableHighlight onPress={() => console.log("Image tapped")}>
-    //     {/* <Image source={require("./assets/icon.png")} /> */}
-    //     <Image
-    //       blurRadius={2}
-    //       // fadeDuration={1000}
-    //       style={styles.logo}
-    //       source={{ width: 200, height: 200, uri: "https://picsum.photos/200" }}
-    //     />
-    //   </TouchableHighlight>
-    //   <TouchableNativeFeedback onPress={() => console.log("Image tapped")}>
-    //     <View
-    //       style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}
-    //     ></View>
-    //   </TouchableNativeFeedback>
-    //   <StatusBar style="auto" />
-    // </SafeAreaView>
-    // <SafeAreaView style={[styles.container, containerStyle]}>
-    //   <Button
-    //     color="dimgrey"
-    //     title="Click Me"
-    //     // onPress={() =>
-    //     //   Alert.alert("My title", "My message", [
-    //     //     { text: "Yes", onPress: () => console.log("Yes") },
-    //     //     { text: "No", onPress: () => console.log("No") },
-    //     //   ])
-    //     // }
-    //     onPress={() =>
-    //       Alert.prompt("My title", "My message", (text) => console.log(text))
-    //     }
-    //   />
-    //   <StatusBar style="auto" />
-    // </SafeAreaView>
-    <SafeAreaView style={styles.container}>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row", //row, row-reverse, column, column-reverse
+        justifyContent: "center", //flex-start, flex-end, center, space-around, space-evenly, space-between
+        alignItems: "center", //baseline, stretch
+        // alignContent: "center",
+        // flexWrap: "wrap",
+      }}>
       <View
         style={{
           backgroundColor: "dodgerblue",
-          // width: 150,
-          width: "50%",
-          height: 70,
-        }}></View>
-    </SafeAreaView>
+          flex: -1,
+          // flexBasis: 100, // can map to width or height depen on primary axis
+          // flexGrow: 1,
+          // flexShrink: 1,
+          width: 400,
+          height: 100,
+          // alignSelf: "flex-start",
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          // flex: 2,
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          // flex: 1,
+          width: 100,
+          height: 100,
+        }}
+      />
+      {/* <View
+        style={{
+          backgroundColor: "grey",
+          // flex: 1,
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "greenyellow",
+          // flex: 1,
+          width: 100,
+          height: 100,
+        }}
+      /> */}
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
-    // paddingTop: Platform.OS === "android" ? 20 : 0,
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
-
-const containerStyle = { backgroundColor: "orange" };
